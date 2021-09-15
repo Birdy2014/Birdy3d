94e8ee8dbe6a0dc9b9751f41373f9db30a176f02
Moritz Vogel <moritzv7@googlemail.com>
2021-09-15 17:19:28 +0200

    chore: add shader uniform setters with const char* parameters
    This slightly improves performance, because string literals don't create
    temporary strings.

--- engine/src/render/Shader.cpp
+++ engine/src/render/Shader.cpp
@@ -133,6 +133,54 @@ namespace Birdy3d {
         glUseProgram(ID);
     }
 
+    void Shader::setBool(const char* name, bool value) const {
+        glUniform1i(glGetUniformLocation(ID, name), (int)value);
+    }
+
+    void Shader::setInt(const char* name, int value) const {
+        glUniform1i(glGetUniformLocation(ID, name), value);
+    }
+
+    void Shader::setFloat(const char* name, float value) const {
+        glUniform1f(glGetUniformLocation(ID, name), value);
+    }
+
+    void Shader::setVec2(const char* name, const glm::vec2& value) const {
+        glUniform2fv(glGetUniformLocation(ID, name), 1, &value[0]);
+    }
+
+    void Shader::setVec2(const char* name, float x, float y) const {
+        glUniform2f(glGetUniformLocation(ID, name), x, y);
+    }
+
+    void Shader::setVec3(const char* name, const glm::vec3& value) const {
+        glUniform3fv(glGetUniformLocation(ID, name), 1, &value[0]);
+    }
+
+    void Shader::setVec3(const char* name, float x, float y, float z) const {
+        glUniform3f(glGetUniformLocation(ID, name), x, y, z);
+    }
+
+    void Shader::setVec4(const char* name, const glm::vec4& value) const {
+        glUniform4fv(glGetUniformLocation(ID, name), 1, &value[0]);
+    }
+
+    void Shader::setVec4(const char* name, float x, float y, float z, float w) const {
+        glUniform4f(glGetUniformLocation(ID, name), x, y, z, w);
+    }
+
+    void Shader::setMat2(const char* name, const glm::mat2& mat) const {
+        glUniformMatrix2fv(glGetUniformLocation(ID, name), 1, GL_FALSE, &mat[0][0]);
+    }
+
+    void Shader::setMat3(const char* name, const glm::mat3& mat) const {
+        glUniformMatrix3fv(glGetUniformLocation(ID, name), 1, GL_FALSE, &mat[0][0]);
+    }
+
+    void Shader::setMat4(const char* name, const glm::mat4& mat) const {
+        glUniformMatrix4fv(glGetUniformLocation(ID, name), 1, GL_FALSE, &mat[0][0]);
+    }
+
     void Shader::setBool(const std::string& name, bool value) const {
         glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
     }
