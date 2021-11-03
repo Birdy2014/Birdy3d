518b704b35983f92f535f5200cdff5e3a81aabed
Moritz Vogel <moritzv7@googlemail.com>
2021-11-03 18:10:39 +0100

    feat(ui): ScrollView only scrolls to end

--- engine/src/ui/widgets/ScrollView.cpp
+++ engine/src/ui/widgets/ScrollView.cpp
@@ -17,17 +17,20 @@ namespace Birdy3d {
         glm::vec2 minsize(m_padding[0] + m_padding[1], m_padding[2] + m_padding[3]);
         if (m_layout && m_children_visible)
             minsize += m_layout->minimal_size(m_children);
-        glm::vec2 content_size = glm::vec2(glm::max(minsize.x, size.x), glm::max(minsize.y, size.y));
+        m_content_size = glm::vec2(glm::max(minsize.x, size.x), glm::max(minsize.y, size.y));
+        m_max_scroll_offset = m_content_size - m_actual_size;
+        m_max_scroll_offset.x = std::max(m_max_scroll_offset.x, 0.0f);
+        m_max_scroll_offset.y = std::max(m_max_scroll_offset.y, 0.0f);
 
         // NOTE: This can probably be removed once the y coordinate is flipped
         glm::vec2 pos_offset = glm::vec2(0, std::min(size.y - minsize.y, 0.0f));
 
         for (const auto& s : m_shapes) {
-            s->parent_size(content_size);
+            s->parent_size(m_content_size);
         }
 
         if (m_layout && m_children_visible)
-            m_layout->arrange(m_children, pos + glm::vec2(m_padding[0], m_padding[2]) + m_scroll_offset + pos_offset, content_size - glm::vec2(m_padding[0] + m_padding[1], m_padding[2] + m_padding[3]));
+            m_layout->arrange(m_children, pos + glm::vec2(m_padding[0], m_padding[2]) + m_scroll_offset + pos_offset, m_content_size - glm::vec2(m_padding[0] + m_padding[1], m_padding[2] + m_padding[3]));
 
         if (resized)
             on_resize();
@@ -38,8 +41,14 @@ namespace Birdy3d {
         m_scroll_offset.x -= event.xoffset * acceleration;
         m_scroll_offset.y -= event.yoffset * acceleration;
 
+        if (m_scroll_offset.x < 0)
+            m_scroll_offset.x = 0;
+        if (m_scroll_offset.x > m_max_scroll_offset.x)
+            m_scroll_offset.x = m_max_scroll_offset.x;
         if (m_scroll_offset.y < 0)
             m_scroll_offset.y = 0;
+        if (m_scroll_offset.y > m_max_scroll_offset.y)
+            m_scroll_offset.y = m_max_scroll_offset.y;
     }
 
 }
--- engine/src/ui/widgets/ScrollView.hpp
+++ engine/src/ui/widgets/ScrollView.hpp
@@ -11,6 +11,8 @@ namespace Birdy3d {
         void arrange(glm::vec2 pos, glm::vec2 size) override;
 
     private:
+        glm::vec2 m_content_size;
+        glm::vec2 m_max_scroll_offset;
         glm::vec2 m_scroll_offset = glm::vec2(0);
 
         void on_scroll(const InputScrollEvent& event) override;
