fa422437044b9786624389ac6fe095b0ee1c93bf
Moritz Vogel <moritzv7@googlemail.com>
2022-02-18 21:11:53 +0100

    refactor(render): use ResourceIdentifier in Material

--- engine/src/render/Material.cpp
+++ engine/src/render/Material.cpp
@@ -4,24 +4,20 @@
 
 namespace Birdy3d::render {
 
-    void Material::diffuse_map(const std::string& name) {
-        m_diffuse_map_name = name;
-        m_diffuse_map = core::ResourceManager::get_texture(name);
+    void Material::diffuse_map(const core::ResourceIdentifier& id) {
+        m_diffuse_map = id;
     }
 
-    void Material::specular_map(const std::string& name) {
-        m_specular_map_name = name;
-        m_specular_map = core::ResourceManager::get_texture(name);
+    void Material::specular_map(const core::ResourceIdentifier& id) {
+        m_specular_map = id;
     }
 
-    void Material::normal_map(const std::string& name) {
-        m_normal_map_name = name;
-        m_normal_map = core::ResourceManager::get_texture(name);
+    void Material::normal_map(const core::ResourceIdentifier& id) {
+        m_normal_map = id;
     }
 
-    void Material::emissive_map(const std::string& name) {
-        m_emissive_map_name = name;
-        m_emissive_map = core::ResourceManager::get_texture(name);
+    void Material::emissive_map(const core::ResourceIdentifier& id) {
+        m_emissive_map = id;
     }
 
     void Material::use(const Shader& shader) const {
@@ -53,30 +49,20 @@ namespace Birdy3d::render {
     }
 
     void Material::serialize(serializer::Adapter& adapter) {
-        bool loading = adapter.mode() == serializer::Adapter::Mode::LOAD;
-
         adapter("diffuse_map_enabled", diffuse_map_enabled);
         adapter("diffuse_color", diffuse_color);
-        adapter("diffuse_map", m_diffuse_map_name);
-        if (loading)
-            diffuse_map(m_diffuse_map_name);
+        adapter("diffuse_map", m_diffuse_map);
 
         adapter("specular_map_enabled", specular_map_enabled);
         adapter("specular_value", specular_value);
-        adapter("specular_map", m_specular_map_name);
-        if (loading)
-            specular_map(m_specular_map_name);
+        adapter("specular_map", m_specular_map);
 
         adapter("normal_map_enabled", normal_map_enabled);
-        adapter("normal_map", m_normal_map_name);
-        if (loading)
-            normal_map(m_normal_map_name);
+        adapter("normal_map", m_normal_map);
 
         adapter("emissive_map_enabled", emissive_map_enabled);
         adapter("emissive_color", emissive_color);
-        adapter("emissive_map", m_emissive_map_name);
-        if (loading)
-            emissive_map(m_emissive_map_name);
+        adapter("emissive_map", m_emissive_map);
     }
 
     BIRDY3D_REGISTER_TYPE_DEF(Material);
--- engine/src/render/Material.hpp
+++ engine/src/render/Material.hpp
@@ -11,18 +11,18 @@ namespace Birdy3d::render {
     public:
         bool diffuse_map_enabled = false;
         utils::Color diffuse_color = utils::Color::WHITE;
-        void diffuse_map(const std::string&);
+        void diffuse_map(const core::ResourceIdentifier&);
 
         bool specular_map_enabled = false;
         float specular_value = 0.0f;
-        void specular_map(const std::string&);
+        void specular_map(const core::ResourceIdentifier&);
 
         bool normal_map_enabled = false;
-        void normal_map(const std::string&);
+        void normal_map(const core::ResourceIdentifier&);
 
         bool emissive_map_enabled = false;
         utils::Color emissive_color = utils::Color::BLACK;
-        void emissive_map(const std::string&);
+        void emissive_map(const core::ResourceIdentifier&);
 
         void use(const Shader& shader) const;
         bool transparent() const;
@@ -30,13 +30,9 @@ namespace Birdy3d::render {
         void serialize(serializer::Adapter&);
 
     private:
-        std::string m_diffuse_map_name = "color::#ffffff";
         core::ResourceHandle<Texture> m_diffuse_map = core::ResourceManager::get_texture("color::" + utils::Color::WHITE.to_string());
-        std::string m_specular_map_name = "color::#000000";
         core::ResourceHandle<Texture> m_specular_map = core::ResourceManager::get_texture("color::" + utils::Color::BLACK.to_string());
-        std::string m_normal_map_name = "color::#ffffff";
         core::ResourceHandle<Texture> m_normal_map = core::ResourceManager::get_texture("color::" + utils::Color::WHITE.to_string());
-        std::string m_emissive_map_name = "color::#000000";
         core::ResourceHandle<Texture> m_emissive_map = core::ResourceManager::get_texture("color::" + utils::Color::BLACK.to_string());
 
         BIRDY3D_REGISTER_TYPE_DEC(Material);
